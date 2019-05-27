d6497690bddb6303b646690f7416f79b581cddea
katanm <mihai.katan@gmail.com>
2019-05-27 15:11:31 +0300

    Home Work Lesson 4. Update file to GIT

---
+++ String_1.java
@@ -0,0 +1,11 @@
+public class String_1 {
+
+	public static void main (String args []){
+
+	String firstName = "Mihai";
+	String lastName = "Catan";
+
+	System.out.println(firstName + " " + lastName);
+	}
+
+}
