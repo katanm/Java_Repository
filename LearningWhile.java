74bf4afee208867ef478c51862a6da74837ed9aa
katanm <50747307+katanm@users.noreply.github.com>
2019-05-29 18:23:57 +0300

    add HomeWork 7.

---
+++ LearningWhile.java
@@ -0,0 +1,52 @@
+public class LearningWhile {
+
+	public static void main(String[] args){
+
+	int c = 0;
+
+	while (c<=100) {
+	
+		System.out.println("Numarul este " +c);
+		c++;
+		
+	}
+
+	System.out.println("+ Punctul 2.1");	
+
+
+	int i;
+	i=1;
+	while (i<=50) 
+ 	
+	{
+	if(i % 2 ==0) {    	
+		System.out.println("Numarul par este " + i);
+	    
+  	} else {
+	}
+	
+	i++;
+	}
+
+	System.out.println("+ Punctul 2.1");
+
+	
+	// int i1;
+	
+	// while ((50 <= i1)||(i1 <= 100)) {
+	// if(i1 % 2 ==0) {    	
+	//	System.out.println("Numarul par este " + i1);
+	    
+  	// } else {
+	// }
+	//i1++;
+	// }
+
+	System.out.println("Numerele impare nu se afiseaza! ");
+		
+	
+
+
+  }
+
+}
